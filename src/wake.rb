152d95c88d5e25d851c6a6b859ebd2730b66e74e
Jason C. Huang <chaoju.huang@gmail.com>
2012-10-22 00:54:05 +0800

    - add Jester to top-player

--- src/wake.rb
+++ src/wake.rb
@@ -1,4 +1,5 @@
 watch('.*\.coffee') {|md| system("coffee -bc -o ..\\js #{md[0]}")}
-watch('.*\.jade')   {|md| system("jade -P -O .. .")}
+watch('.*\.jade')   {|md| system("jade -O .. .")}
+#watch('.*\.jade')   {|md| system("jade -P -O .. .")}
 watch('.*\.styl')   {|md| system("stylus -o ..\\css #{md[0]}")}
 
