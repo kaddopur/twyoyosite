4c4aa6afcbc8cf054aba10e8589d7acc32f14a4d
KeDouBo <kaddo.dev@gmail.com>
2012-11-16 11:10:17 +0800

    Output pretty HTML.

--- src/wake.rb
+++ src/wake.rb
@@ -1,5 +1,6 @@
 watch('.*\.coffee') {|md| system("coffee -bc -o ..\\js #{md[0]}")}
-watch('.*\.jade')   {|md| system("jade -O .. .")}
-#watch('.*\.jade')   {|md| system("jade -P -O .. .")}
+# Produce ugly HTML
+# watch('.*\.jade')   {|md| system("jade -O .. .")}
+watch('.*\.jade')   {|md| system("jade -P -O .. .")}
 watch('.*\.styl')   {|md| system("stylus -o ..\\css #{md[0]}")}
 
